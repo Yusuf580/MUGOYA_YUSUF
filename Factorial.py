a8451869efe4dbb813f28e0dd6fd705df478d7c3
Yusuf580 <yusufandrew580@gmail.com>
2025-05-23 12:42:49 +0300

    Factorial of 5 assignment

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+#Factorial of 5 assignment
+
+def factorial(n):
+    if n==1 or n==0:
+        return 1
+    else:
+        result = n * factorial(n-1)
+        return result
+    
+print("The factorial of five is:",factorial(5))
